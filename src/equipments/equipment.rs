2e53da0026be6756af8e6bc2b58b318097df78c8
Lepage Nathan <nathan.lepage@reseau.eseo.fr>
2024-11-25 09:55:41 +0100

    Ajout des équipements

--- src/equipments/equipment.rs
+++ src/equipments/equipment.rs
@@ -1,17 +1,66 @@
-#[derive(Debug, PartialEq)]
+use serde::Deserialize;
+use std::fs;
+use std::collections::HashMap;
+
+#[derive(Debug, PartialEq, Eq, Hash, Deserialize, Clone)]
 pub enum EquipmentType {
-    Mysterious,
     Hat,
+    Glasses,
     Vest,
     Pants,
     Shoes,
     Whip,
 }
 
-pub trait Equipment {
-    fn get_name(&self) -> String;
-    fn get_icon(&self) -> char;
-    fn get_description(&self) -> String;
-    fn get_position(&self) -> (usize, usize);
-    fn get_type(&self) -> EquipmentType;
+#[derive(Deserialize)]
+struct EquipmentData {
+    name: String,
+    icon: String,
+    description: String,
+}
+#[derive(Debug)]
+pub struct Equipment {
+    name: String,
+    icon: String,
+    description: String,
+    equipment_type: EquipmentType,
+    position: (usize, usize),
+}
+
+const FILE_PATH: &str = "./src/equipments/equipment_data.json";
+
+impl Equipment {
+    pub fn new(equipment_type: EquipmentType, position: (usize, usize)) -> Equipment {
+        let data = fs::read_to_string(FILE_PATH).expect("Unable to read file");
+        let equipment_map: HashMap<EquipmentType, EquipmentData> = serde_json::from_str(&data).expect("JSON was not well-formatted");
+        let equipment_data = equipment_map.get(&equipment_type).expect("Equipment type not found");
+
+        Equipment {
+            name: equipment_data.name.clone(),
+            icon: equipment_data.icon.clone(),
+            description: equipment_data.description.clone(),
+            equipment_type,
+            position,
+        }
+    }
+
+    pub fn get_name(&self) -> String {
+        self.name.to_string()
+    }
+
+    pub fn get_icon(&self) -> char {
+        self.icon.chars().next().unwrap()
+    }
+
+    pub fn get_description(&self) -> String {
+        self.description.to_string()
+    }
+
+    pub fn get_position(&self) -> (usize, usize) {
+        self.position
+    }
+
+    pub fn get_type(&self) -> EquipmentType {
+        self.equipment_type.clone()
+    }
 }
--- src/equipments/mod.rs
+++ src/equipments/mod.rs
@@ -1,7 +1,1 @@
-pub mod hat;
-pub mod vest;
-pub mod pants;
-pub mod shoes;
-pub mod whip;
 pub mod equipment;
-pub mod mysterious;

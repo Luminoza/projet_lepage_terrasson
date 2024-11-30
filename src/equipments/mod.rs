13ced4b5bfcaecced2c6145aaa5d116302f24e16
TERRASSON Antonin <antonin.terrasson@reseau.eseo.fr>
2024-11-30 16:52:06 +0100

    Ajout des commentaires, toujours plus plus

--- src/equipments/mod.rs
+++ src/equipments/mod.rs
@@ -1,1 +1,8 @@
+/**
+ * Module des équipements
+ * Utile pour gérer les équipements du jeu
+ * 
+ * Auteur : Nathan LEPAGE
+ */
+
 pub mod equipment;
--- src/items/mod.rs
+++ src/items/mod.rs
@@ -1,1 +1,8 @@
+/**
+ * Module des items
+ * Utile pour gérer les items du jeu
+ * 
+ * Auteur : Nathan LEPAGE
+ */
+
 pub mod item;
